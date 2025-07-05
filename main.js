9e6388f0c6b1cf617bea062ac57f8e7dd5c32b5c
CristianBarrios983 <barrioscristian.9908@gmail.com>
2025-07-05 18:38:00 -0300

    agregar productos a array y boton de eliminar productos

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const formulario = document.getElementById('formulario-productos');
 const productosDiv = document.getElementById('productos');
 const calcularBoton = document.getElementById('calcular');
 
-let productos = [];
+let productos = []; //Array para almacenar los productos
 
-calcularBoton.style.display = 'none';
+calcularBoton.style.display = 'none'; //Oculta el boton de calcular inicialmente
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -25,25 +25,49 @@ formulario.addEventListener('submit', (e) => {
         precioVenta: precioVenta
     }
 
-    productos.push(producto);
+    productos.push(producto); //Agrega el producto al array de productos
 
-    const productoHTML = `
-        <div class="producto">
-                <label for="">${nombreProducto} <span class="precio-compra">$${precioCompra}</span> <span>${cantidadUnidades} uni.</span> <span class="precio">$${precioVenta}</span></label>
-        </div>
-    `;
-
-    // productosDiv.innerHTML += productoHTML;
-    calcularBoton.insertAdjacentHTML('beforebegin', productoHTML);
-
-    calcularBoton.style.display = 'block'; 
+    mostrarProductos(); //Muestra los productos en el div de productos
     
     document.getElementById('nombre-producto').value = '';
     document.getElementById('precio-compra').value = '';
     document.getElementById('cantidad-unidades').value = '';
     document.getElementById('precio-venta').value = '';
 
-    console.log(productos);
+    // console.log(productos);
 
 
-});
+})
+
+
+function mostrarProductos(){
+    productosDiv.innerHTML = '';
+    let productosHTML = '';
+
+    productos.forEach((producto, index) => {
+        productosHTML += `
+        <div class="producto">
+                <label for="">${producto.nombreProducto} <span class="precio-compra">$${producto.precioCompra}</span> <span>${producto.cantidadUnidades} uni.</span> <span class="precio">$${producto.precioVenta}</span></label>
+                <button class="eliminar-producto" onclick=eliminarProducto(${index})>x</button>
+        </div>
+        `;
+    });
+
+    productosDiv.innerHTML = productosHTML;
+    productosDiv.appendChild(calcularBoton); //Agrega el boton de calcular despues de agregar los productos
+    
+    calcularBoton.style.display = 'block'; //Muestra el boton de calcular al agregar un producto
+
+
+}
+
+
+function eliminarProducto (index) {
+    productos.splice(index, 1);
+    productosDiv.innerHTML = '';
+    mostrarProductos();
+
+    if (productos.length === 0) {
+        calcularBoton.style.display = 'none';
+    }
+} 
