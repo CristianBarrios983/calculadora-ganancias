956c1c7653837bbb20a44871a3520bf5c86cde33
CristianBarrios983 <barrioscristian.9908@gmail.com>
2025-07-05 23:47:02 -0300

    alculo de resumen de ganancias

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-function calcular() {
-    alert('Calculado con Exito');
-}
-
 const formulario = document.getElementById('formulario-productos');
 const productosDiv = document.getElementById('productos');
 const calcularBoton = document.getElementById('calcular');
@@ -16,12 +12,14 @@ formulario.addEventListener('submit', (e) => {
     const nombreProducto = document.getElementById('nombre-producto').value;
     const precioCompra = document.getElementById('precio-compra').value;
     const cantidadUnidades = document.getElementById('cantidad-unidades').value;
+    const unidadesPorPaquete = document.getElementById('unidades-paquete').value;
     const precioVenta = document.getElementById('precio-venta').value;
 
     const producto = {
         nombreProducto: nombreProducto,
         precioCompra: precioCompra,
         cantidadUnidades: cantidadUnidades,
+        unidadesPorPaquete: unidadesPorPaquete,
         precioVenta: precioVenta
     }
 
@@ -32,6 +30,7 @@ formulario.addEventListener('submit', (e) => {
     document.getElementById('nombre-producto').value = '';
     document.getElementById('precio-compra').value = '';
     document.getElementById('cantidad-unidades').value = '';
+    document.getElementById('unidades-paquete').value = '';
     document.getElementById('precio-venta').value = '';
 
     // console.log(productos);
@@ -47,7 +46,7 @@ function mostrarProductos(){
     productos.forEach((producto, index) => {
         productosHTML += `
         <div class="producto">
-                <label for="">${producto.nombreProducto} <span class="precio-compra">$${producto.precioCompra}</span> <span>${producto.cantidadUnidades} uni.</span> <span class="precio">$${producto.precioVenta}</span></label>
+                <label for=""><span>X${producto.cantidadUnidades}</span> ${producto.nombreProducto} <span class="precio-compra">$${producto.precioCompra}</span> <span>${producto.unidadesPorPaquete} uni.</span> <span class="precio">$${producto.precioVenta}</span></label>
                 <button class="eliminar-producto" onclick=eliminarProducto(${index})>x</button>
         </div>
         `;
@@ -71,3 +70,53 @@ function eliminarProducto (index) {
         calcularBoton.style.display = 'none';
     }
 } 
+
+
+function calcular() {
+    let inversionTotal = 0;
+    let ventaTotal = 0;
+    let gananciaTotal = 0;
+
+    let resumenHTML =  `<h2 style="text-align: center;">Resumen de ganancias</h2>`;
+
+    productos.forEach((producto) => {
+        const inversion = parseFloat(producto.precioCompra) * parseInt(producto.cantidadUnidades);
+        const venta = (parseFloat(producto.precioVenta) * parseInt(producto.unidadesPorPaquete)) * producto.cantidadUnidades;
+        const ganancia = venta - inversion;
+        const porcentajeGanancia = (ganancia / inversion) *100;
+        const gananciaPorUnidad = parseFloat(producto.precioVenta) - (parseFloat(producto.precioCompra) / parseInt(producto.unidadesPorPaquete));
+        const porcentajeDeGananciaPorUnidad = ((gananciaPorUnidad / (parseFloat(producto.precioCompra) / parseInt(producto.unidadesPorPaquete))) * 100).toFixed(2);
+
+
+        resumenHTML += `
+            <div>
+                <p><strong>${producto.nombreProducto}</strong></p>
+                <p>Cantidad: ${producto.cantidadUnidades} unidades</p>
+                <p>Inversion: $${inversion.toFixed(2)}</p>
+                <p>Venta: $${venta.toFixed(2)}</p>
+                <p>Ganancia: $${ganancia.toFixed(2)}</p>
+                <p>Ganancia por unidad: $${gananciaPorUnidad.toFixed(2)} (${porcentajeDeGananciaPorUnidad}%)</p>
+            </div>
+        `;
+
+        inversionTotal += inversion;
+        ventaTotal += venta;
+        gananciaTotal += ganancia;
+
+    });
+
+    const porcentajeDeGananciaTotal = ((gananciaTotal / inversionTotal) * 100).toFixed(2);
+
+    resumenHTML += `
+            <div>
+                <h2 style="text-align: center;">Totales</h2>
+                <p>Inversion total: $${inversionTotal.toFixed(2)}</p>
+                <p>Venta total: $${ventaTotal.toFixed(2)}</p>
+                <p>Ganancia total: $${gananciaTotal.toFixed(2)}</p>
+                <p>Porcentaje de ganancia total: ${porcentajeDeGananciaTotal}%</p>
+            </div>
+        `;
+
+    document.getElementById('resumen').innerHTML = resumenHTML;
+}
+
